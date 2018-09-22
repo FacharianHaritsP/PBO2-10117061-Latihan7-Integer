f6f719b44af37aa127d1742c79d392d358623567
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-09-22 19:08:58 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117061/latihan7/integer/PBO210117061Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan7.integer;
+
+/**
+ *
+ * @author Fachrian Harits Pratama
+ * Nama                 : Fachrian Harits Pratama
+ * Kelas                : PBO2
+ * NIM                  : 10117061
+ * Deskripsi program    : Program ini berisi program  untuk menampilkan jenis -
+ *                        jenis tipe data bilangan bulat
+ */
+public class PBO210117061Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       byte b;
+       short s;
+       int i;
+       long l;
+       b = 120;
+       s = 32767;
+       i = 1_000_000_000;
+       l = 90000000000l;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
